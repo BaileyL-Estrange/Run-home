3e76529c0207b163052d4ad4505ba8e36a3808df
Bailey <734019@newcollege.ac.uk>
2026-05-01 11:54:40 +0100

    Update childMovement.cs

--- Assets/SCRIPTS/childMovement.cs
+++ Assets/SCRIPTS/childMovement.cs
@@ -37,20 +37,36 @@ public class childMovement : MonoBehaviour
     private void Patroling()
     {
         if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //When reached walkpoint
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
     }
     private void SearchWalkPoint()
     {
         float randomZ = Random.Range(-walkPointRange, walkPointRange);
         float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
     }
 
     private void ChasePlayer()
     {
-        
+        agent.SetDestination(player.position);
     }
 
     private void WatchingPlayer()
     {
+        agent.SetDestination(transform.position);
 
+        transform.LookAt(player);
     }
 }
